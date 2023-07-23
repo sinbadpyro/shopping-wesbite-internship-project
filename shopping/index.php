aac051c0f85cd7238773d3bb9852822e93f0bb0a
sinbadpyro <omorodionsinbad@gmail.com>
2023-07-23 23:58:21 +0200

    shopping

---
+++ shopping/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--FONT AWESOME-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--BOOTSTRAP CDN-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Shopping Cart</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row text-center py-5">
+            <div class="col-md-3 col-sm-6 my-3 my-md-0">
+                <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="./upload/product1.jpg" alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                A four star handbag, it is amazing.
+                            </p>
+                            <h5>
+                                <small><s class="text-secondary">€400</s></small>
+                                <span class="price">€300</span>
+                            </h5>
+                            <button class="btn btn-warning my-3" type="submit" name="add">Add to Cart <i class="fas fa-shopping-cart"></i>
+                            </button>
+                        </div>
+                </form>
+
+            </div>
+
+
+
+        </div>
+        <div class="col-md-3 col-sm-6 my-3 my-md-0">
+        <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="./upload/product1.jpg" alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                A four star handbag, it is amazing.
+                            </p>
+                            <h5>
+                                <small><s class="text-secondary">€400</s></small>
+                                <span class="price">€300</span>
+                            </h5>
+                            <button class="btn btn-warning my-3" type="submit" name="add">Add to Cart <i class="fas fa-shopping-cart"></i>
+                            </button>
+                        </div>
+                </form>
+        </div>
+        
+       
+    </div>
+    <div class="col-md-3 col-sm-6 my-3 my-md-0">
+    <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="./upload/product1.jpg" alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                A four star handbag, it is amazing.
+                            </p>
+                            <h5>
+                                <small><s class="text-secondary">€400</s></small>
+                                <span class="price">€300</span>
+                            </h5>
+                            <button class="btn btn-warning my-3" type="submit" name="add">Add to Cart <i class="fas fa-shopping-cart"></i>
+                            </button>
+                        </div>
+                </form>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3 my-md-0">
+    <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="./upload/product1.jpg" alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                A four star handbag, it is amazing.
+                            </p>
+                            <h5>
+                                <small><s class="text-secondary">€400</s></small>
+                                <span class="price">€300</span>
+                            </h5>
+                            <button class="btn btn-warning my-3" type="submit" name="add">Add to Cart <i class="fas fa-shopping-cart"></i>
+                            </button>
+                        </div>
+                </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
