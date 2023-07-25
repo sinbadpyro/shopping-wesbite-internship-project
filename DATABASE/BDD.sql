eec6a4f060f6755bbbc0fffd60867e0b7ed18011
sinbadpyro <omorodionsinbad@gmail.com>
2023-07-25 13:21:20 +0200

    shopping cart

---
+++ DATABASE/BDD.sql
@@ -0,0 +1,5 @@
+INSERT INTO producttb(product_name, product_price, product_image)
+    VALUES('Gucci-Bag', 100, './upload/product1.jpg'),
+	('Shirt', 200, './upload/product2.jpg'),
+      ('Nike-shoes', 150, './upload/product3.jpg'),
+       ('Dress', 160, './upload/product4.jpg')
